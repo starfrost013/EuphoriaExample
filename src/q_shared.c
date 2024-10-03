93c0782921ed7a3e8e087caadb1aca5d30841363
starfrost013 <mario64crashed@gmail.com>
2024-10-03 22:55:13 +0100

    add client, server and system specific interface functions

--- src/q_shared.c
+++ src/q_shared.c
@@ -21,16 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "q_shared.h"
 
 
-//============================================================================
-//Restore this if it breaks anything
-
-//#ifdef _WIN32
-//#pragma optimize( "", off )
-//#endif
-
-
-//====================================================================================
-
 /*
 ============
 COM_SkipPath
